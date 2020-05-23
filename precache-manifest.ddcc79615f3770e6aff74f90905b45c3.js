9341e83cc5380026ada0f852c53765e80664bf5b
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-05-23 10:52:06 -0700

    Updates

---
+++ precache-manifest.ddcc79615f3770e6aff74f90905b45c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd2bf5f57542cd51515abb39d2cfd62",
+    "url": "/HomeQuarters/index.html"
+  },
+  {
+    "revision": "530522f65dadd6a08461",
+    "url": "/HomeQuarters/static/css/main.84009f74.chunk.css"
+  },
+  {
+    "revision": "28f021679d004fb2fc91",
+    "url": "/HomeQuarters/static/js/2.d831ecf6.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/HomeQuarters/static/js/2.d831ecf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530522f65dadd6a08461",
+    "url": "/HomeQuarters/static/js/main.ac2dfc80.chunk.js"
+  },
+  {
+    "revision": "5e466f7049c4238d5c54",
+    "url": "/HomeQuarters/static/js/runtime-main.215b5ecd.js"
+  }
+]);
