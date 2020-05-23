aba9a99c73f988a126b082bb61a47acd5774d445
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-05-23 10:50:24 -0700

    Responsive Header

--- src/components/CurrentUser/CurrentUser.js
+++ src/components/CurrentUser/CurrentUser.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./CurrentUser.css";
 import { Link } from "react-router-dom";
 import { signOut } from "../../firebaseConfig";
 
 function CurrentUser({ photoURL }) {
-  // const getWindowWidth = () => {
-  //   const width =
-  //     window.innerWidth ||
-  //     document.documentElement.clientWidth ||
-  //     document.body.clientWidth;
-  //   console.log(width);
-  //   return { width };
-  // };
+  const [width, setWidth] = useState(500);
+
+  const getWindowWidth = () => {
+    const windowWidth =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+
+    setWidth(windowWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", getWindowWidth);
+
+    return () => window.removeEventListener("resize", getWindowWidth);
+  }, []);
 
   return (
     <div className="current-user">
       <Link to="/home" className="link-home">
-        <h1 className="title">HomeQuarters</h1>
+        {width > 400 ? (
+          <h1 className="title">HomeQuarters</h1>
+        ) : (
+          <h1 className="title">HQ</h1>
+        )}
       </Link>
       <div className="current-user-actions">
         <button className="sign-out-button" onClick={signOut}>
